e8b10ea29cb2ba5d96796e124cb47bc124b5d508
afdesk <work@afdesk.com>
2024-11-27 13:22:39 +0600

    add 2 namespaces and new limited role

--- limited-user/deploy.sh
+++ limited-user/deploy.sh
@@ -7,14 +7,23 @@ kubectl wait --for=condition=Ready nodes --all --timeout=300s
 kubectl create namespace rbac-test
 kubectl create -f multiimage-pod.yaml
 
-kubectl create serviceaccount limiteduser -n rbac-test
+kubectl create namespace work
+kubectl create -f single-pod.yaml
+
+kubectl create namespace dev
+kubectl create -f single-pod-dev.yaml
+
+kubectl create serviceaccount limiteduser
 
 kubectl create -f limited-role.yaml
 kubectl create -f limited-binding.yaml
 
+kubectl create -f limited-role-dev.yaml
+kubectl create -f limited-binding-dev.yaml
+
 CA=$(grep 'certificate-authority-data' ~/.kube/config | awk '{print $2}')
 URL=$(grep 'server:' ~/.kube/config | awk '{print $2}')
-TOKEN=$(kubectl create token limiteduser --duration=8760h -n rbac-test)
+TOKEN=$(kubectl create token limiteduser --duration=8760h)
 
 echo "CA: $CA"
 echo "URL: $URL"
@@ -25,4 +34,8 @@ sed -i '' "s|{{CA}}|$CA|g" myconfig
 sed -i '' "s|{{URL}}|$URL|g" myconfig
 sed -i '' "s|{{TOKEN}}|$TOKEN|g" myconfig
 
-trivy k8s -d --report summary --kubeconfig myconfig
+echo "check access to pods"
+kubectl get pods -n rbac-test --kubeconfig myconfig
+
+echo "Run Trivy scan"
+trivy k8s -d --report summary --kubeconfig myconfig mycontext
